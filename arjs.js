a34da342096ca7ffbafb8ad860425fdd709c1d5a
Ignacio-C-Garcia <74923349+Ignacio-C-Garcia@users.noreply.github.com>
2022-06-01 12:14:47 -0300

    demo gaby

--- arjs.js
+++ arjs.js
@@ -20,7 +20,7 @@ AFRAME.registerComponent('markerhandler', {
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-                window.location.href="https://www.delftstack.com/es/howto/javascript/javascript-redirect/#:~:text=JavaScript%20redirecciona%20una%20página%20web%20con%20location.,-href()&text=El%20truco%20consiste%20en%20reemplazar,por%20lo%20tanto%2C%20la%20redireccionará."
+                window.location.href="https://www.youtube.com/watch?v=zqLEO5tIuYs"
             }
         });
 }});
