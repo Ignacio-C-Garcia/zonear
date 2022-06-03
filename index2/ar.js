174b1eff6127cb1d28ccd6a230e9583d0911d3a2
Ignacio-C-Garcia <74923349+Ignacio-C-Garcia@users.noreply.github.com>
2022-06-03 11:43:50 -0300

    Update ar.js

--- index2/ar.js
+++ index2/ar.js
@@ -1,6 +1,6 @@
-var options = {
+/* var options = {
     enableHighAccuracy: true,
-    timeout: 50000,
+    timeout: 5000,
     maximumAge: 0
   };
   
@@ -17,4 +17,55 @@ var options = {
     console.log("no geo")
   };
   navigator.geolocation.getCurrentPosition(success, error, options);
-  
+
+  AFRAME.registerComponent('star', {
+
+    init: function() {
+        
+}});
+*/
+/*
+var id, target, option;
+
+function success(pos) {
+  var crd = pos.coords;
+
+  if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+    console.log('Congratulation, you reach the target');
+    navigator.geolocation.clearWatch(id);
+  }
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+target = {
+  latitude : 0,
+  longitude: 0,
+}
+
+options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+id = navigator.geolocation.watchPosition(success, error, options);
+console.log(id)
+*/
+
+AFRAME.registerComponent('checker', {
+  init: function () {
+    // `this.el` is the element.
+    // `object3D` is the three.js object.
+
+    // `rotation` is a three.js Euler using radians. `quaternion` also available.
+    navigator.geolocation.getCurrentPosition(function(location){
+      console.log(location)
+    })
+
+    // `position` is a three.js Vector3.
+    console.log(this.el.object3D.position);
+  }
+});
