8e81457d9686bd614bc5b88a6e6905f3d8993d26
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-06-16 18:20:51 -0300

    url

--- geolocation/coffee/ar.js
+++ geolocation/coffee/ar.js
@@ -4,7 +4,7 @@ AFRAME.registerComponent('obj_rojo', {
  },
  init() {
    this.el.addEventListener('click', () => {
-       window.location.href="https://www.delftstack.com/es/howto/javascript/javascript-redirect/#:~:text=JavaScript%20redirecciona%20una%20página%20web%20con%20location.,-href()&text=El%20truco%20consiste%20en%20reemplazar,por%20lo%20tanto%2C%20la%20redireccionará."
+       window.location.href="http://zone-ar.rocks/nacho.html"
    });
  }
 });
@@ -14,7 +14,7 @@ AFRAME.registerComponent('obj_amarillo', {
  },
  init() {
    this.el.addEventListener('click', () => {
-       window.location.href="https://www.delftstack.com/es/howto/javascript/javascript-redirect/#:~:text=JavaScript%20redirecciona%20una%20página%20web%20con%20location.,-href()&text=El%20truco%20consiste%20en%20reemplazar,por%20lo%20tanto%2C%20la%20redireccionará."
+       window.location.href="http://zone-ar.rocks/diego.html"
    });
  }
 });
@@ -24,7 +24,7 @@ AFRAME.registerComponent('obj_verde', {
  },
  init() {
    this.el.addEventListener('click', () => {
-       window.location.href="https://www.delftstack.com/es/howto/javascript/javascript-redirect/#:~:text=JavaScript%20redirecciona%20una%20página%20web%20con%20location.,-href()&text=El%20truco%20consiste%20en%20reemplazar,por%20lo%20tanto%2C%20la%20redireccionará."
+       window.location.href="http://zone-ar.rocks/gaby.html"
    });
  }
 });
