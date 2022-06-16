772d9e2d11aefbe077fd0fe9a082dcd662b84301
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-06-16 17:34:29 -0300

    amonguses

--- geolocation/coffee/ar.js
+++ geolocation/coffee/ar.js
@@ -1,44 +1,30 @@
 AFRAME.registerComponent('amongus_rojo', {
-
-  init: function() {
-      const animatedMarker = document.querySelector("#asset_rojo");
-      const aEntity = document.querySelector("#amongus_rojo");
-
-      // every click, we make our model grow in size :)
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              window.location.href="http://zone-ar.rocks/nacho.html"
-          }
-      });
-}});
-
+  schema: {
+   default: ''
+ },
+ init() {
+   this.el.addEventListener('click', () => {
+       window.location.href="https://www.delftstack.com/es/howto/javascript/javascript-redirect/#:~:text=JavaScript%20redirecciona%20una%20página%20web%20con%20location.,-href()&text=El%20truco%20consiste%20en%20reemplazar,por%20lo%20tanto%2C%20la%20redireccionará."
+   });
+ }
+});
 AFRAME.registerComponent('amongus_amarillo', {
-
-  init: function() {
-    const animatedMarker = document.querySelector("#asset_amarillo");
-    const aEntity = document.querySelector("#amongus_amarillo");
-
-      // every click, we make our model grow in size :)
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              window.location.href="http://zone-ar.rocks/cami.html"
-          }
-      });
-}});
-
+  schema: {
+   default: ''
+ },
+ init() {
+   this.el.addEventListener('click', () => {
+       window.location.href="https://www.delftstack.com/es/howto/javascript/javascript-redirect/#:~:text=JavaScript%20redirecciona%20una%20página%20web%20con%20location.,-href()&text=El%20truco%20consiste%20en%20reemplazar,por%20lo%20tanto%2C%20la%20redireccionará."
+   });
+ }
+});
 AFRAME.registerComponent('amongus_verde', {
-
-  init: function() {
-    const animatedMarker = document.querySelector("#asset_verde");
-    const aEntity = document.querySelector("#amongus_verde");
-
-      // every click, we make our model grow in size :)
-      animatedMarker.addEventListener('click', function(ev, target){
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          if (aEntity && intersectedElement === aEntity) {
-              window.location.href="http://zone-ar.rocks/diego.html"
-          }
-      });
-}});
+  schema: {
+   default: ''
+ },
+ init() {
+   this.el.addEventListener('click', () => {
+       window.location.href="https://www.delftstack.com/es/howto/javascript/javascript-redirect/#:~:text=JavaScript%20redirecciona%20una%20página%20web%20con%20location.,-href()&text=El%20truco%20consiste%20en%20reemplazar,por%20lo%20tanto%2C%20la%20redireccionará."
+   });
+ }
+});
