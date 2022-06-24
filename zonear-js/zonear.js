8421bc780cdaa454dbbeb0e3799aab9ef8561de3
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-06-24 10:49:04 -0300

    zonear with js

---
+++ zonear-js/zonear.js
@@ -0,0 +1,46 @@
+function createEntity(Lat, Long, url) {
+    let Entity = document.createElement("a-entity");
+    Entity.setAttribute("animation", "property: rotation; to: 0 -360 0; loop: true; dur: 8000");
+    Entity.setAttribute("link", url);
+    Entity.setAttribute("gltf-model", "#zonear_logo")
+    Entity.setAttribute("scale","2 2 2");
+
+    let coordenates =  "";
+    coordenates.concat("latitude:", Lat, "; longitude: ", Long, ";")
+    Entity.setAttribute("gps-projected-entity-place", coordenates);
+
+    let Scene = document.getElementById("a-scene")
+    console.log("siiiiii")
+    Scene.appendChild(Entity);
+  }
+axios.get('http://localhost:3002/api/get', {
+    responseType: 'json'
+  })
+    .then(function(res) {
+      if(res.status==200) {
+        objarray = res.data;
+
+        for (entity of objarray)
+        {
+            console.log(entity)
+            createEntity(entity.longitude, entity.latitude, entity.url);
+        }
+      }
+    })
+    
+    /*
+let Marker = document.createElement("a-marker");
+Marker.setAttribute("type", "pattern");
+Marker.setAttribute("preset", "kanji");
+Marker.setAttribute("emitevents", "true");
+//Marker.setAttribute("");
+
+let Entity = document.createElement("a-entity");
+Entity.setAttribute("geometry", "primitive: box");
+Entity.setAttribute("material", "color: red");
+
+
+let Camera = document.getElementById("a-camera");
+Marker.appendChild(Entity);
+Camera.appendChild(Marker);
+*/
