1126f9ccea9d15306982cbaabc293fb7a4bca814
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-06-24 10:46:21 -0300

    zonear file

---
+++ zonear/README.md
@@ -0,0 +1,1 @@
+# ar-projectsjyr0f
---
+++ zonear/geolocation/coffee copy/ar.js
@@ -0,0 +1,31 @@
+/*AFRAME.registerComponent('obj_rojo', {
+  schema: {
+   default: ''
+ },
+ init() {
+   this.el.addEventListener('click', () => {
+       window.location.href="http://zone-ar.rocks/nacho.html"
+   });
+ }
+});
+AFRAME.registerComponent('obj_amarillo', {
+  schema: {
+   default: ''
+ },
+ init() {
+   this.el.addEventListener('click', () => {
+       window.location.href="http://zone-ar.rocks/diego.html"
+   });
+ }
+});
+AFRAME.registerComponent('obj_verde', {
+  schema: {
+   default: ''
+ },
+ init() {
+   this.el.addEventListener('click', () => {
+       window.location.href="http://zone-ar.rocks/gaby.html"
+   });
+ }
+});
+*/
